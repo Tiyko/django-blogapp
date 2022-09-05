ea696b94d115a84428bbcd7c11dc35e1a80e29aa
Tiyko <johnny86shepherd@gmail.com>
2022-09-05 12:50:58 +0000

    adjust db link

--- codestar/settings.py
+++ codestar/settings.py
@@ -113,16 +113,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
 
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
         'default': dj_database_url.parse(config('DATABASE_URL'))
     }
 
